384d16a88d0b0605f80dac6a29733db32cccdd7d
adminn <Deepak061996>
2022-05-31 18:42:42 +0530

    changeUi1

--- app/src/main/java/org/com/smartmashinapp/Schedule.java
+++ app/src/main/java/org/com/smartmashinapp/Schedule.java
@@ -3,12 +3,19 @@ package org.com.smartmashinapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import org.com.smartmashinapp.databinding.ActivityMainBinding;
+import org.com.smartmashinapp.databinding.ActivityScheduleBinding;
 
 public class Schedule extends AppCompatActivity {
 
+    private ActivityScheduleBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_schedule);
+        binding= ActivityScheduleBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
